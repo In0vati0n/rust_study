311e69c8b7f954632ddad754fea03d298be4dce7
Ryan <coderyrh9236@gmail.com>
2021-09-18 20:16:06 +0800

    add example

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,3 +1,15 @@
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
 fn main() {
     let mut s = String::from("hello");
 
@@ -13,4 +25,17 @@ fn main() {
     let hello = &hello_world[..5];
     let world = &hello_world[6..];
     println!("1 {} 2 {}", hello, world);
+
+    // ----- string slices -----
+    let my_string = String::from("hello world");
+
+    // both ok
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world"; // string slices
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+    let word = first_word(my_string_literal);
 }
