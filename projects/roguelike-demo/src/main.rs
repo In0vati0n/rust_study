73531fe2472a5c96d0555b0185387cc14f78c5a8
ryan <coderyrh9236@gmail.com>
2021-10-15 23:24:25 +0800

    add rogulike-project

---
+++ projects/roguelike-demo/src/main.rs
@@ -0,0 +1,28 @@
+use quicksilver::{
+    geom::{Rectangle, Vector},
+    graphics::Color,
+    run, Graphics, Input, Result, Settings, Window,
+};
+
+fn main() {
+    run(
+        Settings {
+            title: "Roguelike",
+            size: Vector { x: 800.0, y: 600.0 },
+            ..Settings::default()
+        },
+        app,
+    );
+}
+
+async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+    gfx.clear(Color::WHITE);
+
+    let rect = Rectangle::new(Vector::new(350.0, 100.0), Vector::new(100.0, 100.0));
+    gfx.fill_rect(&rect, Color::BLUE);
+    gfx.stroke_rect(&rect, Color::RED);
+    gfx.present(&window)?;
+    loop {
+        while let Some(_) = input.next_event().await {}
+    }
+}
